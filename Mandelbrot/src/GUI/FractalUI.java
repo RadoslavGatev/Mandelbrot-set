9356529d76421f60d613e2e885f7df0d20d29a8d
RadoslavGatev <radoslavgatev@gmail.com>
2015-06-13 09:14:24 +0300

    Some UI changes

--- Mandelbrot/src/GUI/FractalUI.java
+++ Mandelbrot/src/GUI/FractalUI.java
@@ -12,6 +12,9 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 
 import fractals.FractalRenderer;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout;
+import java.awt.Window.Type;
 
 /**
  *
@@ -23,6 +26,7 @@ public class FractalUI extends javax.swing.JFrame {
 	 * Creates new form FractalUI
 	 */
 	public FractalUI() {
+		setResizable(false);
 		initComponents();
 		generate = arguments1.getGenerateButton();
 		sizeW = arguments1.getWidthField();
@@ -43,10 +47,12 @@ public class FractalUI extends javax.swing.JFrame {
 			public void actionPerformed(ActionEvent ae) {
 				int height = Integer.parseInt(sizeH.getText());
 				int width = Integer.parseInt(sizeW.getText());
-				double a[] = new double[] { Integer.parseInt(aStart.getText()),
-						Integer.parseInt(aEnd.getText()) };
-				double b[] = new double[] { Integer.parseInt(bStart.getText()),
-						Integer.parseInt(bEnd.getText()) };
+				double a[] = new double[] {
+						Double.parseDouble(aStart.getText()),
+						Double.parseDouble(aEnd.getText()) };
+				double b[] = new double[] {
+						Double.parseDouble(bStart.getText()),
+						Double.parseDouble(bEnd.getText()) };
 				String name = fileName.getText();
 				int countOfThreads = Integer.parseInt(tasks.getText());
 
@@ -70,32 +76,32 @@ public class FractalUI extends javax.swing.JFrame {
 	private void initComponents() {
 
 		arguments1 = new Arguments();
+		arguments1.getFilenameField().setText("zad21.png");
+		arguments1.getNameField().setText("1");
+		arguments1.getbEndField().setText("2");
+		arguments1.getbStartField().setText("-2");
+		arguments1.getaEndField().setText("2");
+		arguments1.getaStartField().setText("-2");
+		arguments1.getHeightField().setText("480");
+		arguments1.getWidthField().setText("640");
 
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
 		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
 				getContentPane());
+		layout.setHorizontalGroup(
+			layout.createParallelGroup(Alignment.LEADING)
+				.addGroup(layout.createSequentialGroup()
+					.addComponent(arguments1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		layout.setVerticalGroup(
+			layout.createParallelGroup(Alignment.LEADING)
+				.addGroup(layout.createSequentialGroup()
+					.addComponent(arguments1, GroupLayout.PREFERRED_SIZE, 270, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(63, Short.MAX_VALUE))
+		);
 		getContentPane().setLayout(layout);
-		layout.setHorizontalGroup(layout.createParallelGroup(
-				javax.swing.GroupLayout.Alignment.LEADING).addGroup(
-				javax.swing.GroupLayout.Alignment.TRAILING,
-				layout.createSequentialGroup()
-						.addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE,
-								Short.MAX_VALUE)
-						.addComponent(arguments1,
-								javax.swing.GroupLayout.PREFERRED_SIZE,
-								javax.swing.GroupLayout.DEFAULT_SIZE,
-								javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addContainerGap()));
-		layout.setVerticalGroup(layout.createParallelGroup(
-				javax.swing.GroupLayout.Alignment.LEADING).addGroup(
-				layout.createSequentialGroup()
-						.addGap(22, 22, 22)
-						.addComponent(arguments1,
-								javax.swing.GroupLayout.PREFERRED_SIZE,
-								javax.swing.GroupLayout.DEFAULT_SIZE,
-								javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addContainerGap(36, Short.MAX_VALUE)));
 
 		pack();
 	}// </editor-fold>//GEN-END:initComponents
